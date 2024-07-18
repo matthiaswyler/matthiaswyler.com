cbe52beda57110c8ecd8dadeed7addf39bfecbf0
Matthias Wyler <mail@matthiaswyler.com>
2024-07-18 15:07:55 +0200

    add newsletter signup and archive

---
+++ site/snippets/blocks/signup.php
@@ -0,0 +1,30 @@
+<div id="mc_embed_signup" class="card grid-item">
+  <form action="https://studiosirup.us19.list-manage.com/subscribe/post?u=2c2cee0f76a6a750f25583bed&amp;id=4ec3cbc01b&amp;f_id=00f54be4f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self" novalidate="">
+    <input type="hidden" name="u" value="2c2cee0f76a6a750f25583bed">
+    <input type="hidden" name="id" value="4ec3cbc01b">
+    <input type="text" placeholder="Surname" name="NAME" id="mce-NAME" value="" required="">
+    <input type="text" placeholder="Name" name="LASTNAME" id="mce-LASTNAME" value="" required="">
+    <input type="email" placeholder="Email" autocapitalize="off" autocorrect="off" name="EMAIL" id="mce-EMAIL" value="" required="">
+    <div id="merge-interests-100-12302">
+      <ul class="interestgroup_field checkbox-group">
+        <li>
+          <div class="flex" for="group_2">
+            <input type="checkbox" data-dojo-type="dijit/form/CheckBox" id="group_2" name="group[12302][2]" value="1" class="av-checkbox" required="">
+            <label for="group_2">
+              <p>I want to receive emails.</p>
+            </label>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+      <input type="text" name="b_2c2cee0f76a6a750f25583bed_4ec3cbc01b" tabindex="-1" value="">
+    </div>
+    <div>
+      <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Join">
+    </div>
+    <p>
+      <a href="/newsletters">Archive</a>
+    </p>
+  </form>
+</div>
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,26 @@
 <?php snippet('layout', slots: true) ?>
-<div class="text prose">
-  <?= $page->text()->kt() ?>
-</div>
+
+<section class="projects" style="--span: 8;">
+
+</section>
+<section class="life" style="--span: 4;">
+  <div class="columns">
+    <?php foreach ($page->matter()->toLayouts() as $layout) : ?>
+      <?php foreach ($layout->columns() as $column) : ?>
+        <div class="column" style="--span:<?= $column->span() ?>">
+          <?php foreach ($column->blocks() as $block) : ?>
+            <div class="block <?= $block->type() ?>">
+              <?= $block ?>
+            </div>
+          <?php endforeach ?>
+        </div>
+      <?php endforeach ?>
+    <?php endforeach ?>
+  </div>
+
+  <?php snippet('footer') ?>
+</section>
+
+<?php snippet('polysound') ?>
+
 <?php endsnippet() ?>
