4482e9f9cc363961991f9d63ecc45c3f600b3205
Matthias Wyler <mail@matthiaswyler.com>
2025-12-17 10:26:20 +0100

    refactor(css): reorganize CSS imports and update blueprint structure
    - Added new CSS imports for configuration files to enhance modularity and maintainability.
    - Updated the 'now' blueprint to 'notes' for better clarity and organization.
    - Removed obsolete 'now' blueprint and section files to streamline the project structure.
    - Adjusted the 'note' blueprint to reflect changes in icon and section organization.

--- site/templates/now.php
+++ site/templates/now.php
@@ -3,13 +3,14 @@
 <canvas id="trail"></canvas>
 
 
-<section class="projects" style="--span: 8;">
-
-</section>
+<section class="projects" style="--span: 8;"></section>
 
 <section class="life" style="--span: 4;">
-  <div class="text">
-    <?= $page->text()->kt() ?>
+  <div class="text prose">
+    <?php if ($latestNote = kirby()->site()->find('notes')->children()->listed()->sortBy('num', 'desc')->first()) : ?>
+      <p><?= $latestNote->date()->toDate('d.m.Y H:i') ?></p>
+      <?= $latestNote->text()->kt() ?>
+    <?php endif ?>
   </div>
 </section>
 
